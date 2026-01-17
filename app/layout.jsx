3f8b89eb6a25d1a90b16be782d66a189a8508ce5
Kafy <kaify2908@gmail.com>
2026-01-18 04:36:55 +0530

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import { Outfit } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import StoreProvider from "@/app/StoreProvider";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
+
+export const metadata = {
+    title: "K-Sarwar – Research Lab",
+    description: "K-Sarwar is a medical research lab and ecommerce platform for authentic homeopathic medicines.",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${outfit.className} antialiased`}>
+                    <StoreProvider>
+                        <Toaster />
+                        {children}
+                    </StoreProvider>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
+}
