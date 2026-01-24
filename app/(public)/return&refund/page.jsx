e8a53c6fe226b1c3e109b11d8204e81c05743cb2
ksarwar <ksarwar2026@gmail.com>
2026-01-25 00:37:28 +0530

    add placeholder content for policy pages

---
+++ app/(public)/return&refund/page.jsx
@@ -0,0 +1,8 @@
+export default function ReturnRefundPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Return & Refund Policy</h1>
+      <p>Return & refund details will be added soon.</p>
+    </div>
+  );
+}
---
+++ app/(public)/terms&condition/page.jsx
@@ -0,0 +1,8 @@
+export default function TermsConditionPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Terms & Conditions</h1>
+      <p>Terms & conditions content will be added soon.</p>
+    </div>
+  );
+}
