89c14d849bb6b6c66761daa435f8c4e53cdd9639
Ksarwar <ksarwar2026@gmail.com>
2026-03-15 00:02:59 +0530

    Add product schema for SEO

--- app/(public)/product/[productId]/page.jsx
+++ app/(public)/product/[productId]/page.jsx
@@ -1,49 +1,3 @@
-// 'use client'
-// import ProductDescription from "@/components/ProductDescription";
-// import ProductDetails from "@/components/ProductDetails";
-// import { useParams } from "next/navigation";
-// import { useEffect, useState } from "react";
-// import { useSelector } from "react-redux";
-
-// export default function Product() {
-
-//     const { productId } = useParams();
-//     const [product, setProduct] = useState();
-//     const products = useSelector(state => state.product.list);
-
-//     const fetchProduct = async () => {
-//         const product = products.find((product) => product.id === productId);
-//         setProduct(product);
-//     }
-
-//     useEffect(() => {
-//         if (products.length > 0) {
-//             fetchProduct()
-//         }
-//         scrollTo(0, 0)
-//     }, [productId,products]);
-
-//     return (
-//         <div className="mx-6">
-//             <div className="max-w-7xl mx-auto">
-
-//                 {/* Breadcrums */}
-//                 <div className="  text-gray-600 text-sm mt-8 mb-5">
-//                     Home / Products / {product?.category}
-//                 </div>
-
-//                 {/* Product Details */}
-//                 {product && (<ProductDetails product={product} />)}
-
-//                 {/* Description & Reviews */}
-//                 {product && (<ProductDescription product={product} />)}
-//             </div>
-//         </div>
-//     );
-// }
-
-
-//---------------------------updated------------------
 'use client'
 
 import ProductDescription from "@/components/ProductDescription";
@@ -51,49 +5,70 @@ import ProductDetails from "@/components/ProductDetails";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { generateProductSchema } from "@/lib/seo/productSchema";
 
 export default function Product() {
 
-    const { productId } = useParams();
-    const [product, setProduct] = useState(null);
-    const products = useSelector(state => state.product.list);
+const { productId } = useParams();
+const [product, setProduct] = useState(null);
+const products = useSelector(state => state.product.list);
 
-    useEffect(() => {
-        if (products.length > 0 && productId) {
-            const foundProduct = products.find(
-                (item) => item.id === productId
-            );
-            setProduct(foundProduct || null);
-        }
+useEffect(() => {
 
-        window.scrollTo(0, 0);
+if (products.length > 0 && productId) {
 
-    }, [productId, products]);
+  const foundProduct = products.find(
+    (item) => item.id === productId
+  );
 
-    if (!product) {
-        return (
-            <div className="mx-6 mt-20 text-center text-gray-500">
-                Loading product...
-            </div>
-        );
-    }
+  setProduct(foundProduct || null);
 
-    return (
-        <div className="mx-6">
-            <div className="max-w-7xl mx-auto">
+}
 
-                {/* Breadcrumbs */}
-                <div className="text-gray-600 text-sm mt-8 mb-5">
-                    Home / Products / {product.category}
-                </div>
+window.scrollTo(0, 0);
 
-                {/* Product Details */}
-                <ProductDetails product={product} />
+}, [productId, products]);
 
-                {/* Description & Reviews */}
-                <ProductDescription product={product} />
+if (!product) {
 
-            </div>
-        </div>
-    );
+return (
+  <div className="mx-6 mt-20 text-center text-gray-500">
+    Loading product...
+  </div>
+);
+
+}
+
+const schema = generateProductSchema(product);
+
+return (
+
+<div className="mx-6">
+
+  {/* SEO Product Schema */}
+  <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify(schema)
+    }}
+  />
+
+  <div className="max-w-7xl mx-auto">
+
+    {/* Breadcrumbs */}
+    <div className="text-gray-600 text-sm mt-8 mb-5">
+      Home / Products / {product.category}
+    </div>
+
+    {/* Product Details */}
+    <ProductDetails product={product} />
+
+    {/* Description & Reviews */}
+    <ProductDescription product={product} />
+
+  </div>
+
+</div>
+);
+
 }
